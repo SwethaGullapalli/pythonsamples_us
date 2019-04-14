58a9c711c4e8f1e67f68bea3f10f27cf1aa20141
Swetha <swetha.gullapalli9@gmail.com>
2019-04-14 11:42:54 -0700

    first commit

---
+++ Assignment_3.py
@@ -0,0 +1,15 @@
+
+a=33
+b=300
+c=200
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a is equalto b")
+else:
+    print("a is not greater than b")
+
+for i in range(0,20):
+    print(i)
+    for i in range(0,10):
+        print(i)
